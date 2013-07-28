2966fdee868723b7c3e1b210ea8760f69981a6b0
blessdyb <blessdyb@gmail.com>
2013-07-28 17:37:52 +0800

    amend function view to class based view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,28 +1,25 @@
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from polls.models import Poll, Choice
 
 
-def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_poll_list': latest_poll_list
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
 
-def detail(request, poll_id):
-    #try:
-    #    poll = Poll.objects.get(pk=poll_id)
-    #except Poll.DoesNotExist:
-    #    raise Http404
-    poll = get_object_or_404(Poll, pk = poll_id)
-    return render(request, 'polls/detail.html', {'poll': poll})
+    def get_queryset(self):
+        return Poll.objects.order_by('-pub_date')[:5]
 
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk = poll_id)
-    return render(request, 'polls/results.html', {'poll': poll})
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk = poll_id)
